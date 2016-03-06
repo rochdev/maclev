abd9ef882772b0cae1182409635655e40700952d
rochdev <roch.devost@gmail.com>
2016-03-06 12:09:59 -0500

    added postgres config

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,8 @@ module.exports = {
   client: 'pg',
   connection: {
     host     : 'postgres',
-    user     : 'postgres',
-    password : '',
-    database : 'postgres'
+    user     : process.env.POSTGRES_USER || 'postgres',
+    password : process.env.POSTGRES_PASSWORD || '',
+    database : process.env.POSTGRES_DB || process.env.POSTGRES_USER || 'postgres'
   }
 };
